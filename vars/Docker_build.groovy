3aa88bc5ff99b23f39bf558e423022c340856860
mashish1010 <67505931+mashish1010@users.noreply.github.com>
2025-09-19 19:18:27 +0530

    Update Docker_build.groovy

--- vars/Docker_build.groovy
+++ vars/Docker_build.groovy
@@ -1,4 +1,4 @@
-def call (String ImageTag, String, String ProjectName, String DockerHubUser ){
+def call (String ImageTag, String ProjectName, String DockerHubUser ){
  echo "This is building the codes"
  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 
